25ce39c737a6b54add27bd7c2d0cafebf6bb13bf
Max <babyloopro@gmail.com>
2024-01-08 00:57:58 +0100

    refactored: Improved error handling & logging in email sending route.
    - modified: Enhanced SMTP client configuration validation. Now checks if `SMTP_EMAIL` and `SMTP_PASSWORD` are not incorrect but also non-empty before attempting authentication.
    
    - documented: Added detailed comments explaining the behavior of SMTP credential validation and its limitations in detecting incorrectly set (but non-empty) environment variables.
    
    - updated: Improved error management middleware to more accurately report errors intercepted during request processing.
    
    - optimized: Streamlined environment variable checking method to efficiently handle different sources of environment variables, including .env files, Kubernetes configurations, and operating system variables.
    
    - added: TODO comments added to indicate need for further testing on different environment variable sources and SMTP authentication scenarios.
    
    - cleaned up: Minor code cleanup for readability and maintainability.
    
    These changes aim to enhance the robustness and clarity of the email sending functionality, particularly in handling various cases of SMTP authentication failures and environment variable configurations.

--- ContactForm.MinimalAPI/Program.cs
+++ ContactForm.MinimalAPI/Program.cs
@@ -4,7 +4,10 @@ using Microsoft.Extensions.DependencyInjection;
 using MailKit.Net.Smtp;
 using MimeKit;
 using dotenv.net;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Logging;
 
 public partial class Program
@@ -44,8 +47,8 @@ public partial class Program
             catch (Exception ex)
             {
                 context.Response.StatusCode = 500;
-                await context.Response.WriteAsJsonAsync(new { Message = "ERROR: INTERCEPTED BY MIDDLEWARE" });
-                app.Logger.LogError(ex, "ERROR: INTERCEPTED BY MIDDLEWARE");
+                await context.Response.WriteAsJsonAsync(new { Message = "CONTACT FORM ERROR: INTERCEPTED BY MIDDLEWARE" });
+                app.Logger.LogError(ex, "CONTACT FORM ERROR: INTERCEPTED BY MIDDLEWARE");
             }
         });
 
@@ -53,7 +56,13 @@ public partial class Program
         app.UseCors();
         DotEnv.Load();
 
-        // SMTP SERVICE VARIABLE DEFINITION
+        var missingVariables = CheckEnvironmentVariables("SMTP_HOST", "SMTP_PORT", "SMTP_EMAIL", "SMTP_PASSWORD", "RECEPTION_EMAIL");
+        if (missingVariables.Any())
+        {
+            var missingVariablesString = string.Join(", ", missingVariables);
+            throw new InvalidOperationException($"SMTP ENVIRONMENT VARIABLES ARE NOT PROPERLY SET: {missingVariablesString}");
+        }
+
         var smtpHost = Environment.GetEnvironmentVariable("SMTP_HOST");
         var smtpPortString = Environment.GetEnvironmentVariable("SMTP_PORT");
         var smtpEmail = Environment.GetEnvironmentVariable("SMTP_EMAIL");
@@ -77,9 +86,9 @@ public partial class Program
             {
                 // CREATING AND CONFIGURING EMAIL MESSAGE
                 var emailMessage = new MimeMessage();
-                emailMessage.From.Add(new MailboxAddress("", $"{smtpEmail}")); // Sender of email
-                emailMessage.To.Add(new MailboxAddress("", $"{receptionEmail}")); // Receiver of email
-                emailMessage.To.Add(new MailboxAddress("", $"{request.Email}")); // A copy of email for user who uses "Contact Form" service
+                emailMessage.From.Add(new MailboxAddress("", smtpEmail)); // Sender of email
+                emailMessage.To.Add(new MailboxAddress("", receptionEmail)); // Receiver of email
+                emailMessage.To.Add(new MailboxAddress("", request.Email)); // A copy of email for user who uses "Contact Form" service
                 emailMessage.Subject = "New Message from Contact Form";
                 emailMessage.Body = new TextPart("plain")
                 {
@@ -87,31 +96,32 @@ public partial class Program
                 };
 
                 // SENDING MESSAGE VIA SMTP
-                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                using (var client = new SmtpClient())
                 {
-                    //CHECKING IF ENVIRONMENT VARIABLES ARE NULL
-                    //TODO: OPTIMIZE ENVIRONMENT VARIABLE CHECKING BY REPLACING MULTIPLE 'IF' STATEMENTS WITH A MORE ELEGANT AND EFFICIENT SOLUTION
-                    var missingVariables = new List<string>();
-                    if (smtpHost is null) missingVariables.Add("SMTP_HOST");
-                    if (smtpPortString is null) missingVariables.Add("SMTP_PORT");
-                    if (smtpEmail is null) missingVariables.Add("SMTP_EMAIL");
-                    if (smtpPassword is null) missingVariables.Add("SMTP_PASSWORD");
-                    if (receptionEmail is null) missingVariables.Add("RECEPTION_EMAIL");
-
-                    if (missingVariables.Any())
-                    {
-                        var missingVariablesString = string.Join(", ", missingVariables);
-                        throw new InvalidOperationException($"SMTP ENVIRONMENT VARIABLES ARE NOT PROPERLY SET: {missingVariablesString}");
-                    }
-
+                    // CHECK IF SMTP CLIENT CONFIGURATION IS VALID
                     if (!int.TryParse(smtpPortString, out var smtpPort))
                     {
                         throw new FormatException("'SMTP_PORT' IS NOT A VALID NUMBER");
                     }
 
+                    // CONNECTING TO SMTP SERVER
                     await client.ConnectAsync(smtpHost, smtpPort, true);
+
+                    // VALIDATE SMTP CREDENTIALS BEFORE AUTHENTICATING //TODO: NEED MORE TESTING AND OPTIMIZATION (ONCE ON TEN IT SHOWS !)
+                    // EXPLANATION: This section checks if the SMTP_EMAIL and SMTP_PASSWORD environment variables are correctly set. If either is incorrect due to a grammatical mistake, it logs an error and returns a bad request response.
+                    // However, the current issue is that if these variables are incorrectly set (e.g., with wrong but non-empty values), this condition won't be triggered, and authentication failure may occur elsewhere in the process.
+                    // Thus, if the error message "SMTP_EMAIL OR SMTP_PASSWORD IS NOT CORRECTLY SET" does not consistently appear, it may be due to the variables being considered as a simple "Error: authentication failed: UGFzc3dvcmQ6" rather than being incorrectly set.
+                    // Rectifying these additional logs and more detailed error handling might be necessary to capture authentication failures due to incorrect values and to display these logs.
+                    if (string.IsNullOrEmpty(smtpEmail) || string.IsNullOrEmpty(smtpPassword))
+                    {
+                        logger.LogError("CONTACT FORM ERROR: SMTP_EMAIL OR SMTP_PASSWORD IS NOT SET CORRECTLY");
+                        return Results.BadRequest(new { Message = "ERROR: SMTP_EMAIL OR SMTP_PASSWORD IS NOT SET CORRECTLY" });
+                    }
+
+                    // AUTHENTICATING WITH SMTP SERVER
                     await client.AuthenticateAsync(smtpEmail, smtpPassword);
 
+                    // SENDING EMAIL MESSAGE
                     await client.SendAsync(emailMessage);
                     await client.DisconnectAsync(true);
                 }
@@ -119,27 +129,29 @@ public partial class Program
                 // LOGGING SUCCESS OF SENDING
                 logger.LogInformation($"CONTACT FORM SUCCESS: EMAIL SENT TO {smtpEmail?.ToUpper()} & {receptionEmail?.ToUpper()} FROM {request.Email?.ToUpper()}", request.Email);
                 return Results.Ok(new { Message = "SUCCESS: EMAIL SENT" });
-
-            }
-            catch (FormatException ex)
-            {
-                logger.LogError(ex, $"CONTACT FORM ERROR: FAILED TO SEND EMAIL TO {smtpEmail?.ToUpper()} FROM {request.Email?.ToUpper()}", request.Email);
-                return Results.BadRequest(new { Message = $"FORMAT ERROR: {ex.Message}" });
             }
             catch (SmtpCommandException ex)
             {
-                logger.LogError(ex, $"CONTACT FORM ERROR: FAILED TO SEND EMAIL TO {smtpEmail?.ToUpper()} FROM {request.Email?.ToUpper()}", request.Email);
-                return Results.BadRequest(new { Message = $"SMTP COMMAND ERROR: {ex.Message}" });
+                if (ex.Message.Contains("AUTHENTICATION FAILED", StringComparison.OrdinalIgnoreCase))
+                {
+                    logger.LogError(ex, "CONTACT FORM ERROR: AUTHENTICATION FAILED");
+                    return Results.BadRequest(new { Message = "ERROR: AUTHENTICATION FAILED" });
+                }
+                else
+                {
+                    logger.LogError(ex, "CONTACT FORM ERROR: SMTP COMMAND ERROR");
+                    return Results.BadRequest(new { Message = $"SMTP COMMAND ERROR: {ex.Message}" });
+                }
             }
             catch (SmtpProtocolException ex)
             {
-                logger.LogError(ex, $"CONTACT FORM ERROR: FAILED TO SEND EMAIL TO {smtpEmail?.ToUpper()} FROM {request.Email?.ToUpper()}", request.Email);
+                logger.LogError(ex, "CONTACT FORM ERROR: SMTP PROTOCOL ERROR");
                 return Results.BadRequest(new { Message = $"SMTP PROTOCOL ERROR: {ex.Message}" });
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, $"CONTACT FORM ERROR: FAILED TO SEND EMAIL TO {smtpEmail?.ToUpper()} FROM {request.Email?.ToUpper()}", request.Email);
-                return Results.BadRequest(new { Message = $"UNEXPECTED ERROR: {ex.Message}" });
+                logger.LogError(ex, "CONTACT FORM ERROR: FAILED TO SEND EMAIL TO {SmtpEmail} FROM {RequestEmail}", smtpEmail?.ToUpper(), request.Email?.ToUpper());
+                return Results.BadRequest(new { Message = $"ERROR: {ex.Message}" });
             }
         });
 
@@ -147,6 +159,22 @@ public partial class Program
         app.Run();
     }
 
+    // ENVIRONMENT VARIABLE CHECKING | //TODO: NEED MORE TESTING ON DIFFERENT ENVIRONMENT VARIABLES SOURCES (KUBERNETES, OPERATING SYSTEM, ETC.)
+    // EXPLANATION: This method checks whether specified environment variables are present and not empty.
+    // It operates independently of variable source, whether they come from a .env file, Kubernetes, or the operating system.
+    // If any variables are missing or empty, it returns a list of these for appropriate error handling.
+    private static List<string> CheckEnvironmentVariables(params string[] variableNames)
+    {
+        var missingVariables = new List<string>();
+        foreach (var name in variableNames)
+        {
+            if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable(name)))
+            {
+                missingVariables.Add(name);
+            }
+        }
+        return missingVariables;
+    }
 }
 
 // EMAIL REQUEST CLASS
@@ -161,4 +189,3 @@ public class EmailRequest
     [Required]
     public string? Message { get; set; }
 }
-
